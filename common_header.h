634bf958ef10f701290462693cf5d183db93e3fa
AdvancedX <1564092685@qq.com>
2024-07-03 09:33:47 +0800

    First commit

---
+++ common_header.h
@@ -0,0 +1,55 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <stack>
+#include <cstdlib>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <set>
+#include <cassert>
+#include <iomanip>
+#include <ctime>
+#include <list>
+#include <cctype>
+#include <functional>
+#define maxnum 50
+#define Terminator 1
+#define NonTerminator 0
+#define Table_width 20
+using namespace std;
+
+int indexOfNonT(char x);
+
+int indexOfTerm(char x);
+
+
+int indexOf(const vector<char>& v, char x);
+
+void add_unique_item(vector<char>& f, char c);
+
+int typeOf(char x);
+
+void analyze(const char* str, int len = -1);
+
+void input_grammer();
+void pick_up();
+void getFirstN();
+void getFirstP();
+void getFollow();
+void generateTable();
+
+
+void printAllTerminator();
+void printAllNonTerminator();
+void printAllFirstN();
+void printAllFirstP();
+void printAllFollow();
+void printTable();
+
+#pragma once
